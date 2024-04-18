623e12d0fdd2cbc04e80c55e809ab1a75bcf436b
swapnil <koushikjoijode@gmail.com>
2024-04-18 00:11:50 -0400

    Making Final changes

---
+++ TransportationServicePerformanceAnalysis/pyfiles/TableInsertion.py
@@ -0,0 +1,58 @@
+from DatabaseConnector import DatabaseConnector
+import pandas as pd
+
+def insert_in_table(table_name, df, connector):
+    # Function to insert data from a DataFrame into a database table
+
+    # Define batch size for batch insertion
+    batch_size = 1000  # Adjust this value based on your dataset size and system resources
+
+    # Initialize an empty list to store rows for batch insertion
+    rows_to_insert = []
+
+    # Iterate over DataFrame rows
+    for row in df.itertuples():
+        # Append row values to the list
+        rows_to_insert.append((
+            row.vendorname,
+            row.trip_distance,
+            row.pickup_datetime,
+            row.dropoff_datetime,
+            row.pickuplocation,
+            row.dropofflocation,
+            row.passenger_count,
+            row.ratecode,
+            row.store_and_fwd_flag,
+            row.payment_type,
+            row.fare_amount,
+            row.mta_tax,
+            row.improvement_surcharge,
+            row.tip_amount,
+            row.tolls_amount,
+            row.total_amount,
+            row.congestion_surcharge,
+            row.airport_fee,
+            row.car_type
+        ))
+
+        # Check if batch size is reached
+        if len(rows_to_insert) == batch_size:
+            # Bulk insert the batch into the database
+            connector.insert_data(table_name, f'''
+                INSERT INTO {table_name}
+                (vendor_name, trip_distance, pickup_datetime, dropoff_datetime, pickup_location, dropoff_location, passenger_count, rate_code, store_and_fwd_flag, payment_type, fare_amount, mta_tax, improvement_surcharge, tip_amount, tolls_amount, total_amount, congestion_surcharge, airport_fee,car_type)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            ''', rows_to_insert)
+            connector.commit()
+
+            # Clear the list for the next batch
+            rows_to_insert = []
+
+    # Insert any remaining rows
+    if rows_to_insert:
+        connector.insert_data(table_name, f'''
+            INSERT INTO {table_name}
+            (vendor_name, trip_distance, pickup_datetime, dropoff_datetime, pickup_location, dropoff_location, passenger_count, rate_code, store_and_fwd_flag, payment_type, fare_amount, mta_tax, improvement_surcharge, tip_amount, tolls_amount, total_amount, congestion_surcharge, airport_fee,car_type)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            ''', rows_to_insert)
+        connector.commit()
---
+++ TransportationServicePerformanceAnalysis/pyfiles/main.py
@@ -0,0 +1,111 @@
+from DataExtractor import DataExtractor
+from DatabaseConnector import DatabaseConnector
+import pandas as pd
+import TableCreation
+import TableInsertion
+
+'''Extracting Datafiles from the URL'''
+# URL of the webpage containing the Parquet file URLs
+webpage_url = 'https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page'
+
+# Create an instance of DataExtractor
+extractor = DataExtractor(webpage_url)
+
+# Extract data using DataExtractor.extract() method
+green_dataframes, yellow_dataframes, required_year = extractor.extract()
+
+'''Connecting to the server'''
+# Requesting user for Connection details for MS SQL Server
+print('*Please provide the server details for database connection*')
+print('____________________________________________________________')
+db_type = input('Enter the type of database: ').lower()
+server_name = input('Enter the name of the server: ')
+database_name = input('Enter the name of the database: ')
+user_name = input('Enter the name of the user: ')
+password = input('Enter the password for the user: ')
+
+# Store server details in a dictionary
+server_details = {
+    'server': server_name,
+    'database': database_name,
+    'username': user_name,
+    'password': password
+}
+
+# Create a DatabaseConnector instance for MS SQL Server
+mssql_connector = DatabaseConnector(db_type=db_type, **server_details)
+
+# Connect to the database
+mssql_connector.connect()
+
+monthly_combined_data = pd.DataFrame()
+month_list = [df['pickup_datetime'].dt.month.iloc[0] for df in yellow_dataframes]
+# Process the extracted data and insert into the database
+for month in month_list:
+    for df in yellow_dataframes:
+        # Extract month and year
+        pickup_month = (df['pickup_datetime'].dt.month.iloc[0])
+        pickup_year = (df['pickup_datetime'].dt.year.iloc[0])
+
+
+        if pickup_year == required_year and pickup_month == month:
+            df_year = df[(df['pickup_datetime'].dt.year == pickup_year)] #filter the dates by year
+            df_year_month = df_year[(df_year['pickup_datetime'].dt.month == pickup_month)] #filter the dates by month
+            grouped = df.groupby(df_year_month['pickup_datetime'].dt.date)
+
+            # Initialize an empty list to store sampled dataframes
+            sampled_dfs = []
+            yellow_monthly_combined_data = pd.DataFrame()
+            # Sample 1000 rows at random for each group
+            for _, group_df in grouped:
+                if len(group_df) >= 1000:
+                    sampled_df = group_df.sample(n=1000, random_state=42)  # Set random_state for reproducibility
+                else:
+                    sampled_df = group_df  # Keep all rows if less than 1000
+                sampled_dfs.append(sampled_df)
+
+            for new_df in sampled_dfs:
+                yellow_monthly_combined_data = pd.concat([yellow_monthly_combined_data, new_df], ignore_index=True)
+            monthly_combined_data = pd.concat([monthly_combined_data, yellow_monthly_combined_data], ignore_index=True)
+
+        else:
+            continue
+
+    # Process green car data
+    for df in green_dataframes:
+
+        # Extract month and year
+        pickup_month = (df['pickup_datetime'].dt.month.iloc[0])
+        pickup_year = (df['pickup_datetime'].dt.year.iloc[0])
+
+
+        if pickup_year == required_year and pickup_month == month:
+            df_year = df[(df['pickup_datetime'].dt.year == pickup_year)] #filter the dates by year
+            df_year_month = df_year[(df_year['pickup_datetime'].dt.month == pickup_month)] #filter the dates by month
+            grouped = df.groupby(df_year_month['pickup_datetime'].dt.date)
+
+            # Initialize an empty list to store sampled dataframes
+            sampled_dfs = []
+            green_monthly_combined_data = pd.DataFrame()
+
+            # Sample 1000 rows at random for each group
+            for _, group_df in grouped:
+                if len(group_df) >= 1000:
+                    sampled_df = group_df.sample(n=1000, random_state=42)  # Set random_state for reproducibility
+                else:
+                    sampled_df = group_df  # Keep all rows if less than 1000
+                sampled_dfs.append(sampled_df)
+
+            for new_df in sampled_dfs:
+                green_monthly_combined_data = pd.concat([green_monthly_combined_data, new_df], ignore_index=True)
+            monthly_combined_data = pd.concat([monthly_combined_data, green_monthly_combined_data], ignore_index=True)
+
+
+        else:
+            continue
+
+    table_name = TableCreation.create_table(monthly_combined_data,required_year,month,mssql_connector)
+    TableInsertion.insert_in_table(table_name,monthly_combined_data,mssql_connector)
+    monthly_combined_data = pd.DataFrame()
+
+mssql_connector.close()
